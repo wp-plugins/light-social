d678f99bf40f22082084d61a18f47638308c020f
aldenml <aldenml@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-21 16:49:53 +0000

    Initial commit. Stable.
    git-svn-id: https://plugins.svn.wordpress.org/light-social/trunk@185758 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lightsocial.php
@@ -0,0 +1,219 @@
+<?php
+/*
+Plugin Name: Light Social
+Plugin URI: http://www.aldentorres.com/lightsocial-wordpress-plugin/
+Description: Insert a set of social share links at the bottom of each post.
+Author: Alden Torres
+Version: 1.0
+Author URI: http://www.aldentorres.com/
+*/
+/*  Copyright 2009  Alden Torres  (email : aldenml@yahoo.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, see <http://www.gnu.org/licenses/>.
+*/
+
+//add lightsocial custom style
+function lightsocial_stylesheet()
+{
+	echo '<link rel="stylesheet" href="'. get_bloginfo('wpurl') . '/wp-content/plugins/lightsocial/lightsocial.css" type="text/css" media="screen" />';
+}
+
+function code_digg($title, $link, $img_prefix)
+{
+	$code = '';
+
+	$code .= '<div class="lightsocial_element">';
+	$code .= '<a href="http://digg.com/submit?url='.$link.'&amp;title='.$title.'">';
+	$code .= '<img src="'.$img_prefix.'digg.ico" alt="Digg This" title="Digg This" />';
+	$code .= '</a>';
+	$code .= '</div>';
+
+	return $code;
+}
+
+function code_reddit($title, $link, $img_prefix)
+{
+        $code = '';
+
+	$code .= '<div class="lightsocial_element">';
+	$code .= '<a href="http://www.reddit.com/submit?url='.$link.'&amp;title='.$title.'">';
+        $code .= '<img src="'.$img_prefix.'reddit.ico" alt="Reddit This" title="Reddit This" />';
+        $code .= '</a>';
+	$code .= '</div>';
+
+        return $code;
+}
+
+function code_yahoo_buzz($title, $link, $img_prefix)
+{
+        $code = '';
+
+	$code .= '<div class="lightsocial_element">';
+	$code .= '<a href="http://buzz.yahoo.com/buzz?targetUrl='.$link.'&amp;headline='.$title.'">';
+        $code .= '<img src="'.$img_prefix.'yahoo_buzz.ico" alt="Buzz This" title="Buzz This" />';
+        $code .= '</a>';
+	$code .= '</div>';
+
+        return $code;
+}
+
+function code_dzone($title, $link, $img_prefix)
+{
+        $code = '';
+
+	$code .= '<div class="lightsocial_element">';
+        $code .= '<a href="http://www.dzone.com/links/add.html?title='.$title.'&amp;url='.$link.'">';
+        $code .= '<img src="'.$img_prefix.'dzone.ico" alt="Vote on DZone" title="Vote on DZone" />';
+        $code .= '</a>';
+	$code .= '</div>';
+
+        return $code;
+}
+
+function code_facebook($title, $link, $img_prefix)
+{
+        $code = '';
+
+	$code .= '<div class="lightsocial_element">';
+        $code .= '<a href="http://www.facebook.com/sharer.php?t='.$title.'&amp;u='.$link.'">';
+        $code .= '<img src="'.$img_prefix.'facebook.ico" alt="Share on a Facebook" title="Share on a Facebook" />';
+        $code .= '</a>';
+	$code .= '</div>';
+
+        return $code;
+}
+
+function code_delicious($title, $link, $img_prefix)
+{
+        $code = '';
+
+	$code .= '<div class="lightsocial_element">';
+        $code .= '<a href="http://delicious.com/save?title='.$title.'&amp;url='.$link.'">';
+        $code .= '<img src="'.$img_prefix.'delicious.ico" alt="Bookmark this on Delicious" title="Bookmark this on Delicious" />';
+        $code .= '</a>';
+	$code .= '</div>';
+
+        return $code;
+}
+
+function code_dotnetkicks($title, $link, $img_prefix)
+{
+        $code = '';
+
+	$code .= '<div class="lightsocial_element">';
+	$code .= '<a href="http://www.dotnetkicks.com/kick/?title='.$title.'&amp;url='.$link.'">';
+	$code .= '<img src="'.$img_prefix.'dotnetkicks.ico" alt="Kick It on DotNetKicks.com" title="Kick It on DotNetKicks.com" />';
+	$code .= '</a>';
+	$code .= '</div>';
+
+        return $code;
+}
+
+function code_linkedin($title, $link, $img_prefix)
+{
+        $code = '';
+
+	$code .= '<div class="lightsocial_element">';
+        $code .= '<a href="http://www.linkedin.com/shareArticle?mini=true&amp;url='.$link.'&amp;title='.$title.'&amp;summary=&amp;source=">';
+        $code .= '<img src="'.$img_prefix.'linkedin.ico" alt="Share on LinkedIn" title="Share on LinkedIn" />';
+        $code .= '</a>';
+	$code .= '</div>';
+
+        return $code;
+}
+
+function code_technorati($title, $link, $img_prefix)
+{
+        $code = '';
+
+	$code .= '<div class="lightsocial_element">';
+        $code .= '<a href="http://www.technorati.com/faves?add='.$link.'">';
+        $code .= '<img src="'.$img_prefix.'technorati.ico" alt="Bookmark this on Technorati" title="Bookmark this on Technorati" />';
+        $code .= '</a>';
+	$code .= '</div>';
+
+        return $code;
+}
+
+function code_twitter($title, $link, $img_prefix)
+{
+        $code = '';
+
+	$code .= '<div class="lightsocial_element">';
+        $code .= '<a href="http://twitter.com/home?status='.urlencode('Reading '.urldecode($link)).'">';
+        $code .= '<img src="'.$img_prefix.'twitter.ico" alt="Post on Twitter" title="Post on Twitter" />';
+        $code .= '</a>';
+	$code .= '</div>';
+
+        return $code;
+}
+
+//insert lightsocial custom html
+function lightsocial_insert($content)
+{
+	global $wp_query;
+	$post = $wp_query->post; //get post content
+	$id = $post->ID; //get post id
+	$postlink = get_permalink($id); //get post link
+	$title = trim(urlencode($post->post_title)); // get post title
+	$link = split('#', $postlink); //split the link with '#', for comment
+	$link = urlencode($link[0]);
+	$img_prefix = get_bloginfo('wpurl') . '/wp-content/plugins/lightsocial/';
+
+	$code = '';
+
+	$display = true;
+
+	if ($display)
+	{
+		$code .= '<div class="lightsocial_container">';
+
+		//digg
+		$code .= code_digg($title, $link, $img_prefix);
+
+		//reddit
+		$code .= code_reddit($title, $link, $img_prefix);
+
+		//yahoo buzz
+		$code .= code_yahoo_buzz($title, $link, $img_prefix);
+
+		//dzone
+		$code .= code_dzone($title, $link, $img_prefix);
+
+		//facebook
+                $code .= code_facebook($title, $link, $img_prefix);
+
+		//facebook
+                $code .= code_delicious($title, $link, $img_prefix);
+
+		//dotnetkicks
+		$code .= code_dotnetkicks($title, $link, $img_prefix);
+
+		//linkedin
+                $code .= code_linkedin($title, $link, $img_prefix);
+
+		//technorati
+                $code .= code_technorati($title, $link, $img_prefix);
+
+		//twitter
+                $code .= code_twitter($title, $link, $img_prefix);
+
+		$code .= '</div>';
+	}
+
+	return $content . $code;
+}
+
+add_action('wp_head', 'lightsocial_stylesheet');
+add_filter('the_content', 'lightsocial_insert');
---
+++ readme.txt
@@ -0,0 +1,51 @@
+=== Plugin Name ===
+Contributors: aldenml
+Tags: bookmark, bookmarking, bookmarks, digg, reddit, yahoo buzz, dzone, facebook, delicious, dotnetkicks, linkedin, technorati, twitter
+Requires at least: 2.8.6
+Tested up to: 2.9
+Stable tag: 1.0
+
+This is a super lightweight plugin to insert a set of social share links at the bottom of each post, focused on technical blogs.
+
+== Description ==
+
+This is a super lightweight plugin to insert a set of social share links at the bottom of each post, focused on technical blogs.
+
+With this plugin you don't have options to configure. Simply activate or deactivate to show or not the bookmark social links.
+
+This is the list of used social sites:
+
+*   Digg
+*   Reddit
+*   Yahoo Buzz
+*   DZone
+*   Facebook
+*   Delicious
+*   Dotnetkicks
+*   LinkedIn
+*   Technorati
+*   Twitter
+
+The plugin is implemented using the most simplistic html code to ensure fast load and minimal impact on your blog.
+
+For more customizations see: [Light Social WordPress plugin](http://www.aldentorres.com/lightsocial-wordpress-plugin/ "Light Social WordPress plugin home")
+
+== Installation ==
+
+1. Download and extract it
+2. Copy lightsocial folder to the "/wp-content/plugins/" directory
+3. Activate the plugin through the 'Plugins' menu in WordPress
+4. Done
+
+== Frequently Asked Questions ==
+
+= Do I have to configure something? =
+
+No, simply activate or deactivate the plugin.
+
+== Screenshots ==
+
+== Changelog ==
+
+= 1.0 =
+* First and stable version.
